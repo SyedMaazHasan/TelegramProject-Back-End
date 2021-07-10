1689b3da7d724e70ba577a3e94b423b031efedd6
SyedMaazHasan <syed_maaz_hasan@yahoo.com>
2021-07-10 15:12:32 +0530

    added OTP support

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const movies = require("./movieSerice.js");
 const genre = require("./genreService.js");
 const login = require("./login.js");
 const register = require("./register");
 const emails = require("./emails.js");
-
+const OTPService = require("./OTPService");
 var cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use("/movies", movies);
 app.use("/login", login);
 app.use("/register", register);
 app.use("/emails", emails);
+app.use("/OTPService", OTPService);
 app.use(genre);
 
 ////connecting to db///
